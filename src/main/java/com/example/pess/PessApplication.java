c22b7b588ea023c35cb925559081e01cface1fa9
JessKim <official1126@naver.com>
2019-09-24 22:58:07 +0900

    Initial Commit

---
+++ src/main/java/com/example/pess/PessApplication.java
@@ -0,0 +1,13 @@
+package com.example.pess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessApplication.class, args);
+	}
+
+}
